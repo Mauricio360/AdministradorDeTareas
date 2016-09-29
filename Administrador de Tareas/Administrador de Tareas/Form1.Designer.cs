4654a674c60879aa75ed344c415cd71fabefa887
Mauricio360 <mayaur@miumg.edu.gt>
2016-09-28 19:14:24 -0600

    SO 2016 PRIMERO COMMIT

---
+++ Administrador de Tareas/Administrador de Tareas/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Administrador_de_Tareas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listProcesos = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblcant = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnFin = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listProcesos
+            // 
+            this.listProcesos.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader6,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listProcesos.Location = new System.Drawing.Point(12, 12);
+            this.listProcesos.Name = "listProcesos";
+            this.listProcesos.Size = new System.Drawing.Size(802, 407);
+            this.listProcesos.TabIndex = 0;
+            this.listProcesos.UseCompatibleStateImageBehavior = false;
+            this.listProcesos.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "PROCESO";
+            this.columnHeader1.Width = 111;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "ID";
+            this.columnHeader2.Width = 70;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "MEMORIA";
+            this.columnHeader3.Width = 128;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "VENTANA";
+            this.columnHeader6.Width = 215;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "MEMORIA VIRTUAL";
+            this.columnHeader4.Width = 247;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblcant});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 460);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(834, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblcant
+            // 
+            this.lblcant.Name = "lblcant";
+            this.lblcant.Size = new System.Drawing.Size(118, 17);
+            this.lblcant.Text = "toolStripStatusLabel1";
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Administrador_de_Tareas.Properties.Resources.rotate;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(12, 425);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(33, 28);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnFin
+            // 
+            this.btnFin.Image = global::Administrador_de_Tareas.Properties.Resources.garbage;
+            this.btnFin.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnFin.Location = new System.Drawing.Point(685, 425);
+            this.btnFin.Name = "btnFin";
+            this.btnFin.Size = new System.Drawing.Size(129, 32);
+            this.btnFin.TabIndex = 2;
+            this.btnFin.Text = "Finalizar Proceso";
+            this.btnFin.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnFin.UseVisualStyleBackColor = true;
+            this.btnFin.Click += new System.EventHandler(this.btnFin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 482);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnFin);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listProcesos);
+            this.Name = "Form1";
+            this.Text = "ADMINISTRADOR DE TAREAS  GRUPO#5 SO2016B";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listProcesos;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblcant;
+        private System.Windows.Forms.Button btnFin;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Administrador de Tareas/Administrador de Tareas/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Administrador_de_Tareas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            actualizar();
+        }
+
+
+
+        public void actualizar()
+        {
+            listProcesos.Items.Clear();
+
+            foreach (System.Diagnostics.Process p in System.Diagnostics.Process.GetProcesses())
+            {
+                ListViewItem proc = new ListViewItem();
+
+                proc.Text = p.ProcessName;
+                proc.SubItems.Add(p.MainWindowTitle.ToString());
+                proc.SubItems.Add(p.Id.ToString());
+                proc.SubItems.Add(Math.Round(Convert.ToDouble(p.PrivateMemorySize64/1024))+" K");
+                proc.SubItems.Add(Math.Round(Convert.ToDouble(p.VirtualMemorySize64 / 1024)) + " K");
+
+                listProcesos.Items.Add(proc);
+                proc = null;
+            }
+
+            lblcant.Text = listProcesos.Items.Count.ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            actualizar();
+        }
+
+        private void btnFin_Click(object sender, EventArgs e)
+        {
+            if (listProcesos.SelectedItems.Count > 0)
+            {
+                ListViewItem d = listProcesos.SelectedItems[0];
+
+                foreach (System.Diagnostics.Process p in System.Diagnostics.Process.GetProcesses())
+                {
+                    if(p.ProcessName== d.Text)
+                    {
+                        p.Kill();
+                    }
+                }
+                actualizar();
+            }
+           
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            actualizar();
+        }
+
+
+
+        //---------------------------------------
+    }
+}
